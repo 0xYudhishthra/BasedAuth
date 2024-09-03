9889275bb8fbe0792cf69c0eed4746b1273983aa
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-09-03 19:32:33 +0800

    feat: Add ERC6551Registry contract for creating and managing token bound accounts

--- contracts/src/ERC6551Registry.sol
+++ contracts/src/ERC6551Registry.sol
@@ -3,13 +3,61 @@ pragma solidity ^0.8.4;
 
 import "./interfaces/IERC6551Registry.sol";
 
+/// @title ERC6551Registry
+/// @author Luca3
+/// @notice This contract implements the ERC6551 Registry standard for creating and managing token bound accounts
+/// @dev This contract uses assembly for gas optimization and implements the IERC6551Registry interface
 contract ERC6551Registry is IERC6551Registry {
+    /// @notice The event emitted when a new SNARK verifier is set
+    event SnarkVerifierSet(address indexed snarkVerifier);
+
+    /// @notice The event emitted when a new implementation address is set
+    event ImplementationSet(address indexed implementation);
+
+    /// @notice The event emitted when a new admin address is set
+    event AdminSet(address indexed admin);
+
+    /// @notice The address of the implementation contract
+    address public implementation_;
+
+    /// @notice The address of the SNARK verifier contract
+    address public snarkVerifier_;
+
+    /// @notice The address of the admin
+    address public admin_;
+
+    /// @notice The cardUID
+    string public cardUID_;
+
+    /// @notice Modifier to check if the caller is the admin
+    modifier onlyAdmin() {
+        require(msg.sender == admin_, "Only admin can call this function");
+        _;
+    }
+
+    constructor(
+        address _snarkVerifier,
+        address _implementation,
+        address _admin
+    ) {
+        snarkVerifier_ = _snarkVerifier;
+        implementation_ = _implementation;
+        admin_ = _admin;
+    }
+
+    /// @notice Creates a new ERC6551 account
+    /// @dev Uses CREATE2 to deploy a new account contract
+    /// @param salt A unique value to ensure a unique deployment address
+    /// @param chainId The chain ID where the NFT contract is deployed
+    /// @param tokenContract The address of the NFT contract
+    /// @param tokenId The ID of the NFT
+    /// @return The address of the newly created account
     function createAccount(
-        address implementation,
         bytes32 salt,
         uint256 chainId,
         address tokenContract,
-        uint256 tokenId
+        uint256 tokenId,
+        string memory cardUID
     ) external returns (address) {
         assembly {
             // Memory Layout:
@@ -26,6 +74,8 @@ contract ERC6551Registry is IERC6551Registry {
             // 0xAC   chainId (uint256)              (32 bytes)
             // 0xCC   tokenContract (address)        (32 bytes)
             // 0xEC   tokenId (uint256)              (32 bytes)
+            // 0xFC   snarkVerifier (address)       (32 bytes)
+            // 0xBC   cardUID (string)               (32 bytes)
 
             // Silence unused variable warnings
             pop(chainId)
@@ -33,7 +83,7 @@ contract ERC6551Registry is IERC6551Registry {
             // Copy bytecode + constant data to memory
             calldatacopy(0x8c, 0x24, 0x80) // salt, chainId, tokenContract, tokenId
             mstore(0x6c, 0x5af43d82803e903d91602b57fd5bf3) // ERC-1167 footer
-            mstore(0x5d, implementation) // implementation
+            mstore(0x5d, sload(implementation_.slot)) // implementation
             mstore(0x49, 0x3d60ad80600a3d3981f3363d3d373d3d3d363d73) // ERC-1167 constructor + header
 
             // Copy create2 computation data to memory
@@ -42,13 +92,25 @@ contract ERC6551Registry is IERC6551Registry {
             mstore(0x01, shl(96, address())) // registry address
             mstore(0x15, salt) // salt
 
+            // Copy snarkVerifier to memory
+            mstore(0xFC, sload(snarkVerifier_.slot))
+
+            // Copy cardUID to memory
+            mstore(0x10C, cardUID)
+
             // Compute account address
             let computed := keccak256(0x00, 0x55)
 
             // If the account has not yet been deployed
             if iszero(extcodesize(computed)) {
                 // Deploy account contract
-                let deployed := create2(0, 0x55, 0xb7, salt)
+                let deployed := create2(0, 0x55, 0xd7, salt)
+
+                // Initialize the account with the snarkVerifier address
+                let success := call(gas(), deployed, 0, 0, 0x24, 0x20, 0)
+                if iszero(success) {
+                    revert(0, 0)
+                }
 
                 // Revert if the deployment fails
                 if iszero(deployed) {
@@ -65,7 +127,7 @@ contract ERC6551Registry is IERC6551Registry {
                     0x60,
                     // `ERC6551AccountCreated(address,address,bytes32,uint256,address,uint256)`
                     0x79f19b3655ee38b1ce526556b7731a20c8f218fbda4a3990b6cc4172fdf88722,
-                    implementation,
+                    sload(implementation_.slot),
                     tokenContract,
                     tokenId
                 )
@@ -76,12 +138,19 @@ contract ERC6551Registry is IERC6551Registry {
 
             // Otherwise, return the computed account address
             mstore(0x00, shr(96, shl(96, computed)))
+
             return(0x00, 0x20)
         }
     }
 
+    /// @notice Computes the address of an ERC6551 account
+    /// @dev This function doesn't deploy the account, it only computes the address
+    /// @param salt A unique value to ensure a unique deployment address
+    /// @param chainId The chain ID where the NFT contract is deployed
+    /// @param tokenContract The address of the NFT contract
+    /// @param tokenId The ID of the NFT
+    /// @return The computed address of the account
     function account(
-        address implementation,
         bytes32 salt,
         uint256 chainId,
         address tokenContract,
@@ -96,7 +165,7 @@ contract ERC6551Registry is IERC6551Registry {
             // Copy bytecode + constant data to memory
             calldatacopy(0x8c, 0x24, 0x80) // salt, chainId, tokenContract, tokenId
             mstore(0x6c, 0x5af43d82803e903d91602b57fd5bf3) // ERC-1167 footer
-            mstore(0x5d, implementation) // implementation
+            mstore(0x5d, sload(implementation_.slot)) // implementation
             mstore(0x49, 0x3d60ad80600a3d3981f3363d3d373d3d3d363d73) // ERC-1167 constructor + header
 
             // Copy create2 computation data to memory
@@ -111,5 +180,26 @@ contract ERC6551Registry is IERC6551Registry {
             // Return computed account address
             return(0x00, 0x20)
         }
+    }
+
+    /// @notice Sets the address of the SNARK verifier contract
+    /// @param _snarkVerifier The address of the SNARK verifier contract
+    function setSnarkVerifier(address _snarkVerifier) external onlyAdmin {
+        snarkVerifier_ = _snarkVerifier;
+        emit SnarkVerifierSet(_snarkVerifier);
+    }
+
+    /// @notice Sets the address of the implementation contract
+    /// @param _implementation The address of the implementation contract
+    function setImplementation(address _implementation) external onlyAdmin {
+        implementation_ = _implementation;
+        emit ImplementationSet(_implementation);
+    }
+
+    /// @notice Sets the address of the admin
+    /// @param _newAdmin The address of the new admin
+    function setAdmin(address _newAdmin) external onlyAdmin {
+        admin_ = _newAdmin;
+        emit AdminSet(_newAdmin);
     }
 }
